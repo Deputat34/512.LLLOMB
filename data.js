c4574a7438ce5cd4fdeb0f2bf254027e257c9abc
Deputat34 <adeputat2020@gmail.com>
2024-05-31 14:29:42 +0200

    Upload

---
+++ data.js
@@ -0,0 +1,87 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-r1panorama512lombcorner_1---",
+      "name": "R1.Panorama.512.LOMBcorner_1 - Панорама",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.4502386216692962,
+        "pitch": 0.07095812325649575,
+        "fov": 1.2752188746020257
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9204866144606392,
+          "pitch": -0.14330591123025016,
+          "rotation": 0,
+          "target": "1-r2panorama512lombcorner_1---"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-r2panorama512lombcorner_1---",
+      "name": "R2.Panorama.512.LOMBcorner_1 - Панорама",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.03707316094906865,
+        "pitch": 0.21236712938792834,
+        "fov": 1.2752188746020257
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5388631043116767,
+          "pitch": -0.13951333102916053,
+          "rotation": 0,
+          "target": "0-r1panorama512lombcorner_1---"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
